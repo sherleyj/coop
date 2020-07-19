c5115319515c32a9db34af6db9f09beeb277bd80
Sherley Gionfriddo <sgionfriddo@Sherleys-MacBook-Pro.local>
2020-07-18 22:51:06 -0500

    added room creation and redirect to room

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Nav from './Nav';
 import Coop from './Coop';
 import Player from './Player';
 import './App.css';
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route, useHistory } from 'react-router-dom';
 
 
 function App() {
@@ -12,7 +12,7 @@ function App() {
       <Router>
         <Nav/>
         <Switch>
-          <Route path='/' exact component={Home} />
+          <Route path='/' exact component={GameIdForm} />
           <Route path='/:gameid' exact component={Coop} />
           <Route path='/:gameid/player/:id' component={Player} />
         </Switch>
@@ -21,12 +21,29 @@ function App() {
     );
 }
 
-const Home = () => (
-  <div>
-    <h1>Home</h1>
-    
-  </div>
-);
+function GameIdForm() {
+  const [gameId, setGameId] = useState('this-is-my-game');
+  let history = useHistory();
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    history.push(`${gameId}`)
+  }
+
+  const handleChange = (event) => {
+    setGameId(event.target.value);
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input 
+        type="text" 
+        value={gameId}
+        onChange={(event) => {setGameId(event.target.value)}}
+      />
+    </form>
+  );
+}
 
 
 export default App;
