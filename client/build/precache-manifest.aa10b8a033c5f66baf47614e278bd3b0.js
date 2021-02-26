5a35148d2fc16f09eb89733c478a94bc5c561b05
EC2 Default User <ec2-user@ip-10-0-1-220.us-east-2.compute.internal>
2021-02-26 03:38:17 +0000

    adding folders to git so I can pull

---
+++ client/build/precache-manifest.aa10b8a033c5f66baf47614e278bd3b0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca173eb0b0bf3992af4019e34332fd63",
+    "url": "/index.html"
+  },
+  {
+    "revision": "da137b57d6b124687630",
+    "url": "/static/css/main.3f85393a.chunk.css"
+  },
+  {
+    "revision": "44f2be93d7d0cdd290b7",
+    "url": "/static/js/2.dac4ac89.chunk.js"
+  },
+  {
+    "revision": "c7f92eccde8e496c47f87d08777731a5",
+    "url": "/static/js/2.dac4ac89.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da137b57d6b124687630",
+    "url": "/static/js/main.0d8e66da.chunk.js"
+  },
+  {
+    "revision": "527a3240fe2a2c04eba4",
+    "url": "/static/js/runtime-main.77326a8f.js"
+  }
+]);
