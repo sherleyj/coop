a646e3475e5676780d004f287ae4e591997a3e7c
Sherley Gionfriddo <sgionfriddo@Sherleys-MacBook-Pro.local>
2021-02-25 22:56:01 -0600

    can not change num players

--- client/src/App.js
+++ client/src/App.js
@@ -36,28 +36,59 @@ function App() {
 
 function GameIdForm() {
   const [gameId, setGameId] = useState('this-is-my-game');
+  const [numPlayers, setNumPlayers] = useState(3);
   const [game, setGame] = useContext(GameContext);
   // const [gameId, setGameId] = gid;
   // const [game, setGame] = g;
 
   let history = useHistory();
 
+  const createGame = async () => {
+    try {
+      const requestOptions = {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({"gameId": gameId, "numPlayers": numPlayers})
+      };
+  
+      console.log("POST! posting game: ", game);
+  
+      const data = await fetch('/api/createGame', requestOptions);
+      const gameFromAPI = await data.json();
+  
+      console.log("New game created!: ", gameFromAPI);
+  
+    } catch (e) {
+        console.log(e);
+        return <div>Error: {e.message}</div>;
+    }
+  };  
+
   const handleSubmit = (event) => {
     event.preventDefault();
+    createGame();
     history.push(`${gameId}`);
   }
-
-  const handleChange = (event) => {
-    setGameId(event.target.value);
-  }
-
+  
   return (
     <form onSubmit={handleSubmit}>
+      <span>Game Name:</span>
+      <br></br>
       <input 
         type="text" 
         value={gameId}
         onChange={(event) => {setGameId(event.target.value)}}
       />
+      <br></br>
+      <span>Number of Players:</span>
+      <br></br>
+      <input 
+        type="text" 
+        value={numPlayers}
+        onChange={(event) => {setNumPlayers(event.target.value)}}
+      />
+      <br></br>
+      <button className="btn btn-default" type="submit">Submit</button>
     </form>
   );
 }
